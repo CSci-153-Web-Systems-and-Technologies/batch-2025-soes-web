bff4f1d8fe30c92b8b046794d39bebcdcdcddd0b
Shinkrbs <kerbydolamoscs@gmail.com>
2025-10-20 20:39:09 +0800

    Adjust alignment
    Previous alignment made it into column style and not in row style

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,13 @@ const page = () => {
   return (
     <div>
       {/* Section 1 */}
-      <div>
-        <Phone />
+      <div className="w-screen h-[660px] flex flex-col justify-center items-center overflow-hidden">
+        <div className="flex flex-row justify-center items-center gap-16">
+          <div className="w-[550px] h-64 relative"></div>
+          <div className="w-96 h-[500px] relative">
+            <Phone />
+          </div>
+        </div>
       </div>
       Basta Page here
     </div>
