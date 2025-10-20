1ffeaeed5a3445ea348a621782d0386c279e0511
Shinkrbs <kerbydolamoscs@gmail.com>
2025-10-20 20:40:05 +0800

    Adjusted color of footer text

--- app/components/LandingPageFooter.tsx
+++ app/components/LandingPageFooter.tsx
@@ -4,7 +4,7 @@ const LandingPageFooter = () => {
   return (
     <div className="w-screen h-16 px-48 pt-8 bg- border-t border-border inline-flex flex-col justify-start items-start">
       <div className="self-stretch h-6 relative">
-        <div className="text-center text-foreground justify-start font-bold leading-normal">
+        <div className="text-center text-foreground/50 justify-start font-bold leading-normal">
           SOES 2025
         </div>
       </div>
