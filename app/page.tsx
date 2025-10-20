4957cb6cac63af0fac4cab5990582dbab46c9fd3
Shinkrbs <kerbydolamoscs@gmail.com>
2025-10-20 20:49:37 +0800

    Adds initial landing page content
    Populates the initial section of the landing page with a title, description, and phone image.
    This provides a basic visual representation of the platform's purpose and encourages user engagement.

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,21 @@ const page = () => {
       {/* Section 1 */}
       <div className="w-screen h-[660px] flex flex-col justify-center items-center overflow-hidden">
         <div className="flex flex-row justify-center items-center gap-16">
-          <div className="w-[550px] h-64 relative"></div>
+          <div className="w-[550px] h-64 relative">
+            <div className="justify-start text-foreground text-3xl font-bold italic leading-tight gap-10">
+              All Your Elections In One Place.
+            </div>
+            <div className="justify-start text-foreground/50 text-base leading-normal">
+              Streamline your student organizations elections with a secure
+              transparent, and modern platform designed for administrators,
+              voters, and public transparency
+            </div>
+          </div>
           <div className="w-96 h-[500px] relative">
             <Phone />
           </div>
         </div>
       </div>
-      Basta Page here
     </div>
   );
 };
