8c10948b801d0342172f12adcb570de31a387c09
Shinkrbs <kerbydolamoscs@gmail.com>
2025-10-23 16:10:42 +0800

    increase gap

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import React, { use } from "react";
 import Phone from "@/app/components/Phone";
 import BlurText from "@/components/BlurText";
+import { Button } from "@/components/ui/button";
 
 const page = () => {
   const handleAnimationComplete = () => {
@@ -12,7 +13,8 @@ const page = () => {
     <div>
       {/* Section 1 */}
       <div className="w-screen h-[660px] flex flex-col justify-center items-center overflow-hidden">
-        <div className="flex flex-row justify-center items-center gap-20">
+        <div className="flex flex-row justify-center items-center gap-50 width">
+          {/* Left */}
           <div className="w-[600px] h-64 relative flex flex-col gap-5">
             <div className="justify-start text-foreground text-3xl font-bold italic leading-tight gap-10">
               <BlurText
@@ -28,7 +30,13 @@ const page = () => {
               transparent, and modern platform designed for administrators,
               voters, and public transparency
             </div>
+            <div>
+              <Button className="w-44 h-9 bg-green-800 font-bold text-base text-white hover:bg-green-900">
+                Get Started
+              </Button>
+            </div>
           </div>
+          {/* Right */}
           <div className="w-96 h-[500px] relative">
             <Phone />
           </div>
