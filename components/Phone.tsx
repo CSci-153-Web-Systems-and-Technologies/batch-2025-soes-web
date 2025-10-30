36ca245c728abd37e25d72c7047c496e2d5f61f3
Shinkrbs <kerbydolamoscs@gmail.com>
2025-10-30 18:43:52 +0800

    Updates phone component UI
    Enhances the phone component's UI with a new dashboard state.
    Replaces placeholders with actual data representation using icons and styling.
    Integrates a logo image for improved branding.

--- components/Phone.tsx
+++ components/Phone.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { CheckLine, SearchCheck } from "lucide-react";
+import { CheckLine, ChartColumnBig, UserPen, MonitorDot } from "lucide-react";
 import Tilt from "react-parallax-tilt";
+import logo from "@/public/logo.png";
+import Image from "next/image";
 
 interface PhoneProps {
   phoneState: boolean;
@@ -10,7 +12,7 @@ const StateOne = () => {
   return (
     <div className="self-stretch h-[452px] pl-6 pt-6 pb-4 bg-border rounded-[32px] flex flex-col justify-center items-start gap-6">
       <div className="w-40 h-8 inline-flex justify-between items-center">
-        <div className="w-8 h-8 bg-green-800 rounded-[10px]" />
+        <Image src={logo} height={40} width={40} alt="logo" />
         <div className="w-8 h-8 relative bg-foreground/20 rounded-full" />
       </div>
       <div className="w-40 flex-1 flex flex-col justify-start items-start gap-3">
@@ -49,74 +51,69 @@ const StateOne = () => {
 
 const StateTwo = () => {
   return (
-    <div className="self-stretch h-[456px] pt-6 pb-5 bg-green-800 rounded-[32px] inline-flex flex-col justify-start items-center gap-6">
+    <div className="self-stretch h-[456px] pt-6 pb-5 bg-border rounded-[32px] inline-flex flex-col justify-start items-center gap-6">
       <div className="w-40 h-12 relative">
         <div className="w-32 h-12 left-0 top-0 absolute">
-          <div className="w-20 left-0 top-[-2.50px] absolute justify-start text-white text-base font-bold  leading-6">
+          <div className="w-20 left-0 top-[-2.50px] absolute justify-start text-foreground text-base font-bold  leading-6">
             Election Dashboard
           </div>
         </div>
-        <SearchCheck
-          className="w-7 h-8 left-[131.85px] top-[8px] absolute"
-          color="white"
+        <Image
+          src={logo}
+          height={30}
+          width={30}
+          alt="logo"
+          className="left-[131.85px] top-[8px] absolute"
         />
       </div>
       <div className="w-40 flex-1 flex flex-col justify-start items-start gap-4">
-        <div className="self-stretch h-20 px-4 pt-4 bg-white/10 rounded-2xl flex flex-col justify-start items-start gap-3">
+        <div className="self-stretch h-20 px-4 pt-4 bg-green-800/5 rounded-2xl flex flex-col justify-start items-start gap-3">
           <div className="self-stretch h-5 inline-flex justify-between items-center">
             <div className="w-20 h-5 flex justify-start items-start">
-              <div className="justify-start text-white text-sm font-normal leading-5">
+              <div className="justify-start text-foreground text-sm font-normal leading-5">
                 Total Votes
               </div>
             </div>
             <div className="w-4 h-4 relative overflow-hidden">
-              <div className="w-3 h-3 left-[2px] top-[2px] absolute outline outline-offset-[-0.67px] outline-white" />
-              <div className="w-0 h-1.5 left-[12px] top-[6px] absolute outline outline-offset-[-0.67px] outline-white" />
-              <div className="w-0 h-2 left-[8.67px] top-[3.33px] absolute outline outline-offset-[-0.67px] outline-white" />
-              <div className="w-0 h-0.5 left-[5.33px] top-[9.33px] absolute outline outline-offset-[-0.67px] outline-white" />
+              <ChartColumnBig className="w-4 h-4 text-green-700" />
             </div>
           </div>
           <div className="self-stretch h-6 relative">
-            <div className="left-0 top-[-2.50px] absolute justify-start text-white text-base font-bold  leading-6">
+            <div className="left-0 top-[-2.50px] absolute justify-start text-foreground text-base font-bold  leading-6">
               1,247
             </div>
           </div>
         </div>
-        <div className="self-stretch h-20 px-4 pt-4 bg-white/10 rounded-2xl flex flex-col justify-start items-start gap-3">
+        <div className="self-stretch h-20 px-4 pt-4 bg-green-800/5 rounded-2xl flex flex-col justify-start items-start gap-3">
           <div className="self-stretch h-5 inline-flex justify-between items-center">
             <div className="w-20 h-5 flex justify-start items-start">
-              <div className="flex-1 justify-start text-white text-sm font-normal  leading-5">
+              <div className="flex-1 justify-start text-foreground text-sm font-normal  leading-5">
                 Participation
               </div>
             </div>
             <div className="w-4 h-4 relative overflow-hidden">
-              <div className="w-2.5 h-1 left-[1.33px] top-[10px] absolute outline outline-offset-[-0.67px] outline-white" />
-              <div className="w-0.5 h-[5.16px] left-[10.67px] top-[2.09px] absolute outline outline-offset-[-0.67px] outline-white" />
-              <div className="w-0.5 h-1 left-[12.67px] top-[10.09px] absolute outline outline-offset-[-0.67px] outline-white" />
-              <div className="w-1.5 h-1.5 left-[3.33px] top-[2px] absolute outline outline-offset-[-0.67px] outline-white" />
+              <UserPen className="w-4 h-4 text-green-700" />
             </div>
           </div>
           <div className="self-stretch h-6 relative">
-            <div className="left-0 top-[-2.50px] absolute justify-start text-white text-base font-bold  leading-6">
+            <div className="left-0 top-[-2.50px] absolute justify-start text-foreground text-base font-bold  leading-6">
               82.3%
             </div>
           </div>
         </div>
-        <div className="self-stretch h-20 px-4 pt-4 bg-white/10 rounded-2xl flex flex-col justify-start items-start gap-3">
+        <div className="self-stretch h-20 px-4 pt-4 bg-green-800/5 rounded-2xl flex flex-col justify-start items-start gap-3">
           <div className="self-stretch h-5 inline-flex justify-between items-center">
             <div className="w-30 h-5 flex justify-start items-start">
-              <div className="justify-start text-white text-sm font-normal  leading-5">
+              <div className="justify-start text-foreground text-sm font-normal  leading-5">
                 Active Elections
               </div>
             </div>
             <div className="w-4 h-4 relative overflow-hidden">
-              <div className="w-1 h-[2.67px] left-[6px] top-[6.67px] absolute outline outline-offset-[-0.67px] outline-white" />
-              <div className="w-2.5 h-2.5 left-[3.33px] top-[3.33px] absolute outline outline-offset-[-0.67px] outline-white" />
-              <div className="w-3.5 h-0 left-[1.33px] top-[12.67px] absolute outline outline-offset-[-0.67px] outline-white" />
+              <MonitorDot className="w-4 h-4 text-green-700" />
             </div>
           </div>
           <div className="self-stretch h-6 relative">
-            <div className="left-0 top-[-2.50px] absolute justify-start text-white text-base font-bold  leading-6">
+            <div className="left-0 top-[-2.50px] absolute justify-start text-foreground text-base font-bold  leading-6">
               3
             </div>
           </div>
