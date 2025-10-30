1439c6d75b669175d613a849d6bec60447842f10
Shinkrbs <kerbydolamoscs@gmail.com>
2025-10-30 13:18:07 +0800

    Adjust State Two Icon Size

--- components/LandingCard.tsx
+++ components/LandingCard.tsx
@@ -12,12 +12,12 @@ interface LandingCardProps {
   title: string;
   description: string;
   className?: string;
-  type?: "one";
+  type?: "one" | "two";
 }
 
 const LandingCardOne = ({ Icon, title, description }: LandingCardProps) => {
   return (
-    <Card className="hover:shadow-foreground shadow-2xl/20 shadow-background transition-shadow">
+    <Card className="hover:shadow-foreground shadow-2xl/20 shadow-background transition-shadow h-60">
       <CardHeader className="text-center pb-4">
         <div className="w-16 h-16 bg-green-900/10 rounded-2xl flex items-center justify-center mx-auto mb-4">
           <Icon className="w-8 h-8 text-green-800" />
@@ -31,11 +31,11 @@ const LandingCardOne = ({ Icon, title, description }: LandingCardProps) => {
 
 const LandingCardTwo = ({ Icon, title, description }: LandingCardProps) => {
   return (
-    <Card>
+    <Card className="hover:shadow-foreground shadow-2xl/20 shadow-background transition-shadow h-35">
       <CardHeader>
         <div className="flex items-center gap-3">
-          <div className="w-10 h-10 bg-primary/10 rounded-lg flex items-center justify-center">
-            <Icon className="w-8 h-8 text-green-800" />
+          <div className="w-10 h-10 bg-green-900/10 rounded-lg flex items-center justify-center">
+            <Icon className="w-6 h-6 text-green-800" />
           </div>
           <CardTitle className="text-base">{title}</CardTitle>
         </div>
