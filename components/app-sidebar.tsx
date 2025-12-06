dee8e2fe2df5fb5136518dc3d04a6004bf265b33
Shinkrbs <kerbydolamoscs@gmail.com>
2025-12-06 16:58:26 +0800

    Add Sidebar menu items

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import { LayoutDashboard, Vote, Users, Flag, ChartColumn, Settings } from "lucide-react";
 import Image from "next/image";
 import logo from "@/public/logo.svg";
 import React from "react";
@@ -11,36 +11,43 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarGroupLabel,
+  SidebarFooter
 } from "@/components/ui/sidebar";
 
 // Menu items.
 const items = [
   {
-    title: "Home",
+    title: "Dashboard",
     url: "#",
-    icon: Home,
+    icon: LayoutDashboard,
   },
   {
-    title: "Inbox",
+    title: "Elections",
     url: "#",
-    icon: Inbox,
+    icon: Vote,
   },
   {
-    title: "Calendar",
+    title: "Candidates and Partylists",
     url: "#",
-    icon: Calendar,
+    icon: Users,
   },
   {
-    title: "Search",
+    title: "Position Templates",
     url: "#",
-    icon: Search,
+    icon: Flag,
   },
   {
+    title: "Reports and Results",
+    url: "#",
+    icon: ChartColumn,
+  },
+];
+const systemitem =   {
     title: "Settings",
     url: "#",
     icon: Settings,
-  },
-];
+  }; // Settings 
 
 export function AppSidebar() {
   return (
@@ -48,30 +55,46 @@ export function AppSidebar() {
       <SidebarContent>
         <SidebarGroup>
           <SidebarHeader>
-            <div className="w-64 h-20 inline-flex justify-start items-center gap-2 border-b-1 border-neutral-200">
+            <div className="pb-2 h-20 w-[16rem] inline-flex justify-start items-center gap-2 border-b-1 border-neutral-200">
               <Image src={logo} height={45} width={45} alt="logo" />
               <div className="inline-flex flex-col justify-start items-start">
                 <div className="self-stretch h-6 relative">
-                    <h1 className="absolute justify-start font-bold leading-6 text-2xl">SOES</h1>
+                  <h1 className="absolute justify-start font-bold leading-6 text-2xl">
+                    SOES
+                  </h1>
                 </div>
                 <div className="self-stretch h-4 relative">
-                    <h2 className="justify-start text-gray-600 text-[10px] leading-4 font-normal">Student Organization Election System</h2>
+                  <h2 className="justify-start text-gray-600 text-[10px] leading-4 font-normal">
+                    Student Organization Election System
+                  </h2>
                 </div>
               </div>
             </div>
           </SidebarHeader>
           <SidebarGroupContent>
-            <SidebarMenu>
-              {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
+            <SidebarMenu className="flex gap-1">
+              <div className="mb-10">
+                <SidebarGroupLabel>Management</SidebarGroupLabel>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </div>
+              <SidebarGroupLabel>System</SidebarGroupLabel>
+              <SidebarMenuItem key={systemitem.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
+                    <a href={systemitem.url}>
+                      <systemitem.icon />
+                      <span>{systemitem.title}</span>
                     </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
-              ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
